7f997f7eee41fcf93554168ec36a2eab98188bcc
Aprogas <git@aprogas.net>
2013-02-11 16:48:23 +0100

    Convert from optparse to argparse

--- ck2tpcost.py
+++ ck2tpcost.py
@@ -22,7 +22,7 @@
 # 
 
 import sys
-from optparse import OptionParser
+import argparse
 
 def calc_cost(distance, relation):
     '''Calculates trade post cost, expects distance as fractional multiplier'''
@@ -34,35 +34,21 @@ def relation_bribed(relation_current, bribe_effect):
 
 if __name__ == '__main__':
     # Define command-line options
-    parser = OptionParser()
-    parser.add_option('-d', '--distance', action='store', dest='distance')
-    parser.add_option('-r', '--relation', action='store', dest='relation')
-    parser.add_option('-b', '--bribe-cost', action='store', dest='bribe_cost', default=0)
-    parser.add_option('-e', '--bribe-effect', action='store', dest='bribe_effect', default=0)
-    # Read options, check sanity, and normalise
-    (options, args) = parser.parse_args()
-    try:
-        distance = 1 + float(options.distance) / 100.0
-    except:
-        parser.error('Could not parse distance, enter as in game but without + or %, e.g. 102.4')
-    try:
-        relation = float(options.relation)
-    except:
-        parser.error('Could not parse relation, enter as whole number, e.g. -23')
-    try:
-        bribe_cost = float(options.bribe_cost)
-    except:
-        parser.error('Could not parse bribe cost, enter as shown in game, e.g. 133.7')
-    try:
-        bribe_effect = int(options.bribe_effect)
-    except:
-        parser.error('Could not parse bribe effect, enter as whole number, e.g. 42')
+    parser = argparse.ArgumentParser(description='Calculate effectiveness of bribing top liege before building tradeposts.')
+    parser.add_argument('-d', '--distance', type=float, default=0)
+    parser.add_argument('-r', '--relation', type=int, default=0)
+    parser.add_argument('-b', '--bribe-cost', type=float, default=0)
+    parser.add_argument('-e', '--bribe-effect', type=int, default=0)
+    # Read options and normalise
+    args = parser.parse_args()
+    distance_mult = 1 + args.distance / 100.0
     
     # Calculate both scenarios
-    cost1 = calc_cost(distance, relation)
-    cost2 = calc_cost(distance, relation_bribed(relation, bribe_effect))
-    breakeven = bribe_cost / (cost1 - cost2)
+    cost1 = calc_cost(distance_mult, args.relation)
     print "Cost per tradepost without bribe : {:7.2f}".format(cost1)
-    print "Cost per tradepost with bribe    : {:7.2f}".format(cost2)
-    print "Cost for 1 tradepost and bribe   : {:7.2f}".format(cost2 + bribe_cost)
-    print "Tradeposts needed to break even  : {: .2f}".format(breakeven)
+    if args.bribe_effect:
+        cost2 = calc_cost(distance_mult, relation_bribed(args.relation, args.bribe_effect))
+        breakeven = args.bribe_cost / (cost1 - cost2)
+        print "Cost per tradepost with bribe    : {:7.2f}".format(cost2)
+        print "Cost for 1 tradepost and bribe   : {:7.2f}".format(cost2 + args.bribe_cost)
+        print "Tradeposts needed to break even  : {: .2f}".format(breakeven)
